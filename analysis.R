d41d85f2cb6854636c1716223bb39e9d6962cf1a
Bill DeRose <bill.derose11@gmail.com>
2014-02-04 12:44:22 -0800

    Added a few plots regarding NumBcc and NumCc

--- analysis.R
+++ analysis.R
@@ -8,10 +8,18 @@ files <- list.files(pattern="-[a-z]{1}.csv")
 
 # Construct data, add columns
 dataset <- ldply(files[1:2], read.csv, header=T, sep  = ",") # this will take a while
-dataset <- mutate(dataset, Message = as.character(Message))
+dataset <- mutate(dataset, Message = as.character(Message), 
+                  Subject = as.character(Subject), Cc = as.character(Cc),
+                  Bcc = as.character(Bcc))
 dataset <- mutate(dataset, MessageLength = nchar(Message))
-dataset <- mutate(dataset, Date = strptime(dataset[, "Date"], 
-                  format = "%a, %d %b %Y %X"))
+dataset <- mutate(dataset, SubjectLength = nchar(Subject))
+dataset <- mutate(dataset, Date = strptime(Date, format = "%a, %d %b %Y %X"))
+dataset <- mutate(dataset, NumCc = sapply(strsplit(Cc, ","), length),
+                  NumBcc = sapply(strsplit(Bcc, ","), length))
+
+# percentage of replies
+length(grep("RE:", dataset$Subject))/length(dataset$Subject)
+dataset$Subject[grep("RE:", dataset$Subject)] # all subject lines containing "RE:"
 
 edges <- dataset[c("From", "To")] # select all rows, only From, To columns
 g <- graph.data.frame(edges, directed = FALSE)
@@ -22,4 +30,9 @@ plot(g.lc, layout=layout.fruchterman.reingold, vertex.color="gray60",
 
 # Simple plots
 ggplot(dataset) + geom_bar(aes(x = Date$hour), binwidth = 1)
-ggplot(dataset) + geom_bar(aes(x = MessageLength), binwidth = 100)
+ggplot(dataset) + geom_bar(aes(x = MessageLength), binwidth = 1000)
+ggplot(dataset) + geom_bar(aes(x = SubjectLength), binwidth = 10)
+ggplot(dataset) + geom_bar(aes(x = NumCc), binwidth = 1)
+ggplot(dataset) + geom_bar(aes(x = NumBcc), binwidth = 1)
+ggplot(dataset) + geom_point(aes(x = NumBcc, y= NumCc))
+
